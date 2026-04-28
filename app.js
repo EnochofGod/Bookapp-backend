e0c0cafd532343058b077f46c0b4cd586fa938f9
Enoch <adepateenoch@gmail.com>
2026-04-28 16:05:56 +0100

    tc

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ if(process.env.NODE_ENV === 'production'){
     app.use(cors());
 }else{
     app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:5173',
     }))
 }
 
